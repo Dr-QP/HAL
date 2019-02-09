4541dfd91e11c0fd1e85b06941b9a3fdfa2cf35c
Anton Matosov <anton.matosov@gmail.com>
2019-02-08 23:03:54 -0800

    fix if statement

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,7 +6,7 @@ set -x
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew update
     brew upgrade python
-elif [[ -z "$CONAN_DOCKER_IMAGE" ]]
+elif [[ -z "$CONAN_DOCKER_IMAGE" ]]; then
     sudo dpkg --add-architecture i386
     sudo apt update
     sudo apt install -y libc6:i386 libncurses5:i386 libstdc++6:i386 multiarch-support
