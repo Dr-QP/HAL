d3368fe2b06db101c3faf1b07af29848dc6a7abb
Anton Matosov <anton.matosov@gmail.com>
2019-02-08 22:53:14 -0800

    Install compat layer for x86 bins

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,6 +6,10 @@ set -x
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew update
     brew upgrade python
+else
+    dpkg --add-architecture i386
+    apt update
+    apt install -y libc6:i386 libncurses5:i386 libstdc++6:i386 multiarch-support
 fi
 
 pip3 install conan conan_package_tools --upgrade
